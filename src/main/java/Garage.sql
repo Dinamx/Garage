ac298aa6575ae3c8ab86b7631a17cc7e216533d7
priscafehiarisoa <mytyrealy@gmail.com>
2023-01-17 14:58:43 +0300

    creation vues employes details

--- src/main/java/Garage.sql
+++ src/main/java/Garage.sql
@@ -208,6 +208,18 @@ create table Salaire(
 
 -- But ; benefice = vente - depense
 
+-- vues
+create view employedetail as select employe.nom as nom,
+                                    employe.prenom as prenom,
+                                    employe.datenaissance as datenaissance,
+                                    n.description as niveauetude,
+                                    g.genre as genre,
+                                    sal.montant as salairemontant,
+                                    sal.montant/(30/8) as salaireHoraire
+                             from employe
+                                      join niveauetude n on employe.idniveauetude = n.id
+                                      join salaire sal on employe.id = sal.idemploye
+                                      join genre g on employe.idgenre = g.id
 
 
 
